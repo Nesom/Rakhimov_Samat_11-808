74d735c9fb18c5436c1e0c127a25fab9a0c6a190
Nesom <rakhimovsamatarrr@gmail.com>
2019-05-14 13:23:47 +0300

    my kontrl

---
+++ 2019/MISC/ThreadTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ThreadTask
+{
+    class JSONComments
+    {
+        public Comment[] Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        public int Postid { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Body { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = string.Empty;
+            var t = File.ReadAllLines(path);
+            var trtr = new JSONComments
+            {
+
+            };
+            string serialized = JsonConvert.SerializeObject(trtr);
+            JSONComments comments = JsonConvert.DeserializeObject<JSONComments>(serialized);;
+        }
+    }
+}
